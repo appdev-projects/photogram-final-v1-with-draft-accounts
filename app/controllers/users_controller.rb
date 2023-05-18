fa460d9d6ffc7891d448f16e5923b8f53dcdd6a4
Titus Chu <tituschu@uchicago.edu>
2023-05-18 16:52:24 +0000

    2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
 
     render({ :template => "users/index.html.erb" })
   end
+
+  def show
+    the_username = params.fetch("user_name")
+
+    matching_users = User.where({ :username => the_username })
+
+    @the_user = matching_users.at(0)
+
+    render({ :template => "users/show.html.erb" })
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index" })
 
-
+  get("/users/:user_name", { :controller => "users", :action => "show" })
 
   # Routes for the Comment resource:
 
