c27759e67528b01d63217d3947fba1fde6ccc498
Titus Chu <tituschu@uchicago.edu>
2023-05-24 22:05:55 +0000

    7

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -24,11 +24,12 @@ class LikesController < ApplicationController
 
     if the_like.valid?
       the_like.save
-      redirect_to("/likes", { :notice => "Like created successfully." })
+      redirect_to("/photos/#{the_like.photo_id}", { :notice => "Like created successfully." })
     else
-      redirect_to("/likes", { :alert => the_like.errors.full_messages.to_sentence })
+      redirect_to("/photos/#{the_like.photo_id}", { :alert => the_like.errors.full_messages.to_sentence })
     end
   end
+
 
   def update
     the_id = params.fetch("path_id")
@@ -48,9 +49,10 @@ class LikesController < ApplicationController
   def destroy
     the_id = params.fetch("path_id")
     the_like = Like.where({ :id => the_id }).at(0)
+    the_photo_id = the_like.photo_id 
 
     the_like.destroy
 
-    redirect_to("/likes", { :notice => "Like deleted successfully."} )
+    redirect_to("/photos/#{the_photo_id}", { :notice => "Like deleted successfully."} )
   end
 end
